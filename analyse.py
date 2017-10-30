e1fcdd82154ead0214c421aebadec6de37a3210a
kassem <ali.kassem@inria.fr>
2017-10-30 11:27:19 +0100

    init

--- analyse.py
+++ analyse.py
@@ -22,7 +22,7 @@ def get_column(matrix, i): # returns only one colum of the matrix
 def get_range(array, start, end): # simulates the query
     return [array[i] for i in range(len(array)) if array[i] >= start and array[i] <= end]
 
-def get_uniform_pair(option): # select 0 to generate both values independently
+def get_uniform_pair(option, combinations =[]): # select 0 to generate both values independently
     if option == 0:           # other to uniformly select one pair from an array with all possible combinations
         a = int(np.random.uniform(lower, higher))
         b = int(np.random.uniform(lower, higher))
@@ -31,50 +31,42 @@ def get_uniform_pair(option): # select 0 to generate both values independently
         a = int(np.random.uniform(0, len(combinations)))
         return combinations[a]
 
-if __name__ == '__main__':
+def make_predictions(target_col, nb_iterations, n, hist_samples, mla='GNB'):
 
-    start_time = time.time()
-
-    # reduce_database(age_min, age_max, size, filename)
-    reduce_database(20, 90, 100, "data/adult-short.all")
+    # read dataset from a file
     DATA, IS_CAT = read_adult_data()
  
-    target_col = 5  # the number of the column we have information about (5 is age)
-    n = 1           # number of histograms per label
-
     target_attribute = get_column(DATA, target_col) # the column itself
-    combinations = [[a,b] for a in target_attribute for b in target_attribute if(a <= b)]
+    lower = min(target_attribute)    # minimum value for target attribute
+    higher = max(target_attribute)   # maximum value for target attribute
+    target_values = range(lower, higher+1, 1)
+    combinations = [[a,b] for a in target_values for b in target_values if(a <= b)]
 
-    lower = min(target_attribute)    # minimum range for age
-    higher = max(target_attribute)   # maximum range for age
-    width = higher - lower + 1
-
-    # histogram_samples = width**3
-    histogram_samples = 10000 # why width**3? it's pretty effective but it takes ages
-
-    acc = 0
-    Nb_records_O = []
-    for target_row in range(0, 10, 1): # len(target_attribute)):
-        print ("(" + str(target_row) + ") Generating data...")
+    acc = 0.0
+    if nb_iterations == -1:
+        nb_iterations = len(target_attribute)
+    for target_row in range(0, nb_iterations, 1): # len(target_attribute)):
+        print ("(" + str(target_row) + ") Generating training data...")
         actual_value = target_attribute[target_row]  
         training_data = []
         for label in range (lower, higher + 1, 1): # create n histograms for each label
             target_attribute[target_row] = label  
             for j in range(0, n, 1): # create n histograms for label k     
                 histogram = [0] * (len(target_attribute)+1)
-                for i in range(0, histogram_samples, 1): # create one histogram
-                    pair = get_uniform_pair(1) # important: parameter must be changed below to generate the input
+                for i in range(0, hist_samples, 1): # create one histogram
+                    pair = get_uniform_pair(1, combinations)
                     histogram[len(get_range(target_attribute, pair[0], pair[1]))] += 1
                     training_data.append([histogram, label])
-        
         #restore the original database
         target_attribute[target_row] = actual_value
 
+        # generate input
         histogram = [0] * (len(target_attribute)+1)
-        for i in range(0, histogram_samples, 1): # create one histogram
-            pair = get_uniform_pair(1)
+        for i in range(0, hist_samples, 1): # create one histogram
+            pair = get_uniform_pair(1, combinations)
             histogram[len(get_range(target_attribute, pair[0], pair[1]))] += 1
-        input = copy.deepcopy(histogram) # only one of n inputs is considered
+        input = copy.deepcopy(histogram)
+        print input
 
         print "Training classifier..."
         
@@ -83,20 +75,18 @@ if __name__ == '__main__':
         labels = np.array(get_column(training_data, 1))
         
         # train classifier
-        clf = train_classifier(train, labels, mla = 'GNB')  # mla specifies training algorithm to use
+        clf = train_classifier(train, labels, mla)  # mla specifies training algorithm to use
         
         # predict
         predicted_value = clf.predict([input]).tolist()[0]
         
         if actual_value == predicted_value:
-            acc += 1
+            acc += 1.0
             print  bcolors.OKGREEN + "Prediction is correct" + bcolors.ENDC
         else:
             print  bcolors.FAIL + "Prediction is incorrect" + " (" + str(actual_value) + ", " + str(predicted_value) + ")" + bcolors.ENDC
-    
-    elapsed_time = time.time() - start_time
-    
-    print (bcolors.BOLD + 'Accuracy was ' + str(int(acc/float(10)*100)) + '%' + bcolors.ENDC) # the 10 value should depend on the number of iterations!
-    print ('Elapsed time ' + time.strftime("%Hh:%Mm:%Ss", time.gmtime(elapsed_time)))
+
+    a = '%.2f'%(acc*100/float(nb_iterations))
+    print (bcolors.BOLD + 'Accuracy was ' + a + '%' + bcolors.ENDC)
 
 
--- test.py
+++ test.py
@@ -1,53 +1,28 @@
 """
-Test  read_adult_data() and train_classifier() methods. 
+Test make_predictions method
 """
 
 # !/usr/bin/env python
 # coding=utf-8
-from utils.read_data import read_adult_data
-from utils.methods import *
-import numpy as np
-import random 
-import copy
+from analyse import *
 
-        
         
 if __name__ == '__main__':
-    
-    #Read data from a file
-    #IS_CAT specifies the type of the attributes (it is an additional information)
-    DATA, IS_CAT = read_adult_data()
-    #Print first record
-    print DATA[0]
-    
-    #Randomlly select $size lines from data, if size < len(data).  
-    selected_DATA = []
-    length = len(DATA)
-    size = 100
-    temp_DATA = copy.deepcopy(DATA) #copy data
-    if size <= length:
-        counter = 0
-        while(counter < size):
-            i = random.choice(range(len(temp_DATA)))
-            selected_DATA.append(temp_DATA.pop(i))
-            counter += 1
 
-    #Write selected data to a file
-    write_data(selected_DATA, 'data.txt')
 
-    #Generate some random data (1000 records) to train a classifier
-    random_DATA = []
-    for i in range(0, 1000, 1):
-        record = []
-        for i in range(0, 10, 1):
-           rand = random.randint(0, 100)
-           record.append(rand)
-        random_DATA.append(record)
-    #Train a classifier
-    training_DATA = np.array(random_DATA)
-    Label = np.array(100*range(10))
-    clf = train_classifier(training_DATA, Label, mla='GNB')  #$mla specifies training algorithm to use
-    #Use the classifier to obtain a prediction, then a prediction distribution
-    input = [20, 70, 90, 55, 67, 17, 83, 46, 39, 4]
-    print 'Prediction:',  clf.predict([input]).tolist()[0]
-    print 'Prediction Distribution:',  clf.predict_proba([input]).tolist()[0]
+
+    # reduce_database(age_min, age_max, size, filename)
+    reduce_database(20, 25, 100, "data/adult-short.all")
+
+    target_column = 5  # target attribute position (5 is age)
+    Nb_iterations = -1 # nb. of records to consider (-1 to consider all the records)
+    N = 10          # number of histograms per label
+    histogram_samples = 1000 # number of samples per histogram
+    MLA = 'GNB'
+
+    start_time = time.time()
+
+    make_predictions(target_column, Nb_iterations, N, histogram_samples, MLA)
+
+    elapsed_time = time.time() - start_time
+    print ('Elapsed time ' + time.strftime("%Hh:%Mm:%Ss", time.gmtime(elapsed_time)))
